377be403311c65f20adbb73ed5f60ca2c97123cf
Nayan Jain <jainnayan128@gmail.com>
2021-01-13 21:11:47 +0530

    first commit

---
+++ src/main/java/com/postgresqlDB/Controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.postgresqlDB.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String welcomePage(){
+        return  "Welcome, to my page";
+    }
+}
