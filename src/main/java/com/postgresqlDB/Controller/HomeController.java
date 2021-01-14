576b430e3b341f67de65b6b6079a6489f41850ed
Nayan Jain <71127472+NayanJain7@users.noreply.github.com>
2021-01-14 15:07:40 +0530

    Update HomeController.java

--- src/main/java/com/postgresqlDB/Controller/HomeController.java
+++ src/main/java/com/postgresqlDB/Controller/HomeController.java
@@ -1,13 +1,40 @@
 package com.postgresqlDB.Controller;
 
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.postgresqlDB.Dao.UserRepository;
+import com.postgresqlDB.Model.User;
 
 @RestController
 public class HomeController {
 
+	@Autowired UserRepository userRepo;
+	
     @GetMapping("/")
     public String welcomePage(){
+    	User user = new User();
+    	user.setEmail("ajay@gmail.com");
+    	user.setName("Ajay");
+    	user.setPhone("8503158627");
+    	
+    	User save = userRepo.save(user);
+    	
+    	System.out.println("--------------"+save+"----------------");
+    	
         return  "Welcome, to my page";
+    }
+    
+    @GetMapping("/show")
+    public List<User> showDetails(){
+    	
+    	List<User> findAll = userRepo.findAll();
+    	
+    	return findAll;
+    	
     }
 }
